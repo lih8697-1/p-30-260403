bdbbfdee5c72207b270db3aa682f382652617afd
lih8697-1 <lih8697@gmail.com>
2026-04-06 12:37:45 +0900

    (front)26-useAuth 훅으로 분리

--- frontend/src/lib/client.ts
+++ frontend/src/lib/client.ts
@@ -4,18 +4,23 @@ export function fetchApi(url: string, options?: RequestInit) {
   options.credentials = "include";
 
   if (options?.body) {
-    const headers = new Headers(options.headers || {});
-    headers.set("Content-Type", "application/json");
-    options.headers = headers;
+      const headers = new Headers(options.headers || {});
+      headers.set("Content-Type", "application/json");
+      options.headers = headers;
   }
 
   return fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${url}`, options).then(
-    async (res) => {
-      if (!res.ok) {
-        const rsData = await res.json();
-        throw new Error(rsData.msg || "요청 실패");
+      async (res) => {
+          if (!res.ok) { // 200
+              const rsData = await res.json();
+              throw new Error(rsData.msg || "요청 실패");
+          }
+          return res.json();
       }
-      return res.json();
-    }
   )
 }
+
+export type FetchCallbacks = {
+  onSuccess?: (data: any) => void;
+  onError?: (err: any) => void;
+};
